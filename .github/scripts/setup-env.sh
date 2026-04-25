29d17cc955b8477217fda8f077cd08cdcc332504
SuperKali <hello@superkali.me>
2026-04-25 19:43:42 +0200

    fix(setup-env): install python3-setuptools for u-boot prereq
    The OpenWrt prereq check fails with 'Please install the Python3
    setuptools module' because the package wasn't in the apt list.
    Add python3-setuptools to COMMON_PACKAGES so make doesn't bail at
    include/prereq.mk.

--- .github/scripts/setup-env.sh
+++ .github/scripts/setup-env.sh
@@ -59,7 +59,7 @@ bzip2 ccache clang cmake cpio curl device-tree-compiler ecj fastjar flex gawk ge
 gperf haveged help2man intltool libelf-dev libglib2.0-dev libgmp3-dev libltdl-dev libmpc-dev \
 libmpfr-dev libncurses-dev libpython3-dev libreadline-dev libssl-dev libtool libyaml-dev zlib1g-dev \
 lld llvm lrzsz genisoimage nano ninja-build p7zip p7zip-full patch pkgconf python3 python3-pip \
-python3-ply python3-docutils python3-pyelftools qemu-utils re2c rsync scons squashfs-tools \
+python3-ply python3-docutils python3-pyelftools python3-setuptools qemu-utils re2c rsync scons squashfs-tools \
 subversion swig texinfo uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev zstd gh git jq"
 
 X86_64_PACKAGES="gcc-multilib g++-multilib libc6-dev-i386 lib32gcc-s1"
